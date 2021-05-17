2873c61430281b6d9d0ae063927d80b014d37819
MartinLoera <jesusmartinloera@gmail.com>
2021-05-16 19:33:26 -0600

    Hola mundo desde un archivo

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const http = require('http');
+const fs = require('fs');
+
+const data = fs.readFileSync('./WWW/file.txt')
 
 http.createServer((req, res) => {
     res.writeHead(200, {"Content-Type":"text/plain"})
-    res.write("Hola mundo a mi primer servidor")
-
+    res.write(data)
+    res.end()
 }).listen(4444)
+
