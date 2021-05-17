ef7b13509f17f2118d2a30ad94f66db5ad631c49
MartinLoera <jesusmartinloera@gmail.com>
2021-05-16 19:51:34 -0600

    Uso de la extension

--- app.js
+++ app.js
@@ -3,20 +3,38 @@ const fs = require("fs");
 
 //const data = fs.readFileSync('./WWW/file.txt')
 
-http
-  .createServer((req, res) => {
+http.createServer((req, res) => {
     console.log(req.url);
     const file = req.url == "/" ? "./WWW/file.txt" : `./WWW${req.url}`;
     fs.readFile(file, (err, data) => {
-      if (err) {
-        res.writeHead(404, { "Content-Type": "text/plain" });
-        res.write("Not found");
-        res.end();
-      } else {
-        res.writeHead(200, { "Content-Type": "text/plain" });
-        res.write(data);
-        res.end();
-      }
+        if (err) {
+            res.writeHead(404, { "Content-Type": "text/plain" });
+            res.write("Not found");
+            res.end();
+        } else {
+            const extension = file.split(".").pop();
+            switch (extension) {
+                case "txt":
+                    res.writeHead(200, { "Content-Type": "text/plain" });
+                    break;
+
+                case "html":
+                    res.writeHead(200, { "Content-Type": "text/html" });
+                    break;
+                case "jpeg":
+                    res.writeHead(200, { "Content-Type": "image/jpeg" });
+                    break;
+                case "css":
+                    res.writeHead(200, { "Content-Type": "text/css" });
+                    break;
+                case "js":
+                    res.writeHead(200, { "Content-Type": "text/javascript" });
+                    break;
+                default:
+                    break;
+            }
+            res.write(data);
+            res.end();
+        }
     });
-  })
-  .listen(4444);
+}).listen(4444);
