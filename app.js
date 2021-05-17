e57e6342d6c079b4ff841341dc7c3368e36e82f6
MartinLoera <jesusmartinloera@gmail.com>
2021-05-16 19:37:54 -0600

    Uso de url

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
-const http = require('http');
-const fs = require('fs');
+const http = require("http");
+const fs = require("fs");
 
-const data = fs.readFileSync('./WWW/file.txt')
+//const data = fs.readFileSync('./WWW/file.txt')
 
-http.createServer((req, res) => {
-    res.writeHead(200, {"Content-Type":"text/plain"})
-    res.write(data)
-    res.end()
-}).listen(4444)
-
+http
+  .createServer((req, res) => {
+    console.log(req.url);
+    const file = req.url == "/" ? "./WWW/file.txt" : `./WWW${req.url}`;
+    fs.readFile(file, (err, data) => {
+      if (err) {
+        res.writeHead(404, { "Content-Type": "text/plain" });
+        res.write("Not found");
+        res.end();
+      } else {
+        res.writeHead(200, { "Content-Type": "text/plain" });
+        res.write(data);
+        res.end();
+      }
+    });
+  })
+  .listen(4444);
